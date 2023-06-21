3cfee91e76197174f024e989374638fb3f452a91
Akira HIRATA <hirataakira6@gmail.com>
2023-06-21 23:30:13 +0900

    add rabit

---
+++ study/three.js/blender-rabit/index.js
@@ -0,0 +1,61 @@
+window.addEventListener("DOMContentLoaded", init);
+
+function init() {
+    const width = 960;
+    const height = 540;
+
+    // レンダラーを作成
+    const canvasElement = document.querySelector('#myCanvas');
+    const renderer = new THREE.WebGLRenderer({
+        antialias: true,
+        canvas: canvasElement,
+    });
+    renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.setSize(width, height);
+
+    // シーンを作成
+    const scene = new THREE.Scene();
+
+    // カメラを作成
+    const camera = new THREE.PerspectiveCamera(45, width / height, 1, 10000);
+    camera.position.set(0, 0, 200);
+
+    // カメラコントローラーを作成
+    const controls = new THREE.OrbitControls(camera, canvasElement);
+    controls.enableDamping = true;
+    controls.dampingFactor = 0.2;
+
+    // 環境光源を作成
+    const ambientLight = new THREE.AmbientLight(0xffffff);
+    ambientLight.intensity = 0.5;
+    scene.add(ambientLight);
+
+    // 平行光源を作成
+    const directionalLight = new THREE.DirectionalLight(0xffffff);
+    directionalLight.intensity = 1;
+    directionalLight.position.set(1, 3, 1);
+    scene.add(directionalLight);
+
+    // 3Dモデルの読み込み
+    const objLoader = new THREE.OBJLoader();
+    objLoader.load(
+        'rabit.obj',
+        function (obj) {
+            scene.add(obj);
+
+            // モデルのスケールを設定する
+            const scale = new THREE.Vector3(20, 20, 20);
+            obj.scale.copy(scale);
+
+            obj.position.x = 0;
+            obj.position.y = -20;
+        },
+    );
+
+    // レンダリングループ
+    tick();
+    function tick() {
+        renderer.render(scene, camera);
+        requestAnimationFrame(tick);
+    }
+}
--- study/three.js/box/index.js
+++ study/three.js/box/index.js
@@ -1,10 +1,9 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  const width = 960;
-  const height = 540;
 
-  // レンダラーを作成
+  // レンダリング初期設定
+  const [width, height] = [960, 540];
   const renderer = new THREE.WebGLRenderer({
     canvas: document.querySelector('#myCanvas')
   });
@@ -14,35 +13,30 @@ function init() {
   // シーンを作成
   const scene = new THREE.Scene();
 
-  // カメラを作成
+  // カメラ
   const camera = new THREE.PerspectiveCamera(45, width / height, 1, 10000);
-  // カメラの初期座標を設定（X座標:0, Y座標:0, Z座標:0）
-  camera.position.set(0, 0, 1000);
+  camera.position.set(0, 0, 1000); // 初期座標を設定（X座標:0, Y座標:0, Z座標:0）
 
-  // 箱を作成
+  // モデリング: 箱を作成
   const geometry = new THREE.BoxGeometry(500, 500, 500);
   const material = new THREE.MeshStandardMaterial({color: 0x0000FF});
   const box = new THREE.Mesh(geometry, material);
   scene.add(box);
 
-  // 平行光源
+  // ライティング
   const light = new THREE.DirectionalLight(0xFFFFFF);
   light.intensity = 2; // 光の強さを倍に
   light.position.set(1, 1, 1); // ライトの方向
-  // シーンに追加
   scene.add(light);
 
-  // 初回実行
-  tick();
-
-  function tick() {
-    requestAnimationFrame(tick);
+  // アニメーション
+  tickAnimation(); // 初回実行
+  function tickAnimation() {
+    requestAnimationFrame(tickAnimation);
 
     // 箱を回転させる
-    box.rotation.x += 0.01;
-    box.rotation.y += 0.01;
-
-    // レンダリング
+    box.rotation.x += 0.01; box.rotation.y += 0.01;
+    // レンダリング (繰り返される)
     renderer.render(scene, camera);
   }
 }
